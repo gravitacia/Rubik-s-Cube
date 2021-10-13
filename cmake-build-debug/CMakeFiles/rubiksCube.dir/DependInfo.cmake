6217a752e65106ed0cb1282eae326717f1a88ce5
gravitacia <71985277+gravitacia@users.noreply.github.com>
2021-10-13 20:26:41 +0300

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/rubiksCube.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/kosar/CLionProjects/rubiksCube/Blue.cpp" "C:/Users/kosar/CLionProjects/rubiksCube/cmake-build-debug/CMakeFiles/rubiksCube.dir/Blue.cpp.obj"
+  "C:/Users/kosar/CLionProjects/rubiksCube/Cube.cpp" "C:/Users/kosar/CLionProjects/rubiksCube/cmake-build-debug/CMakeFiles/rubiksCube.dir/Cube.cpp.obj"
+  "C:/Users/kosar/CLionProjects/rubiksCube/Green.cpp" "C:/Users/kosar/CLionProjects/rubiksCube/cmake-build-debug/CMakeFiles/rubiksCube.dir/Green.cpp.obj"
+  "C:/Users/kosar/CLionProjects/rubiksCube/Orange.cpp" "C:/Users/kosar/CLionProjects/rubiksCube/cmake-build-debug/CMakeFiles/rubiksCube.dir/Orange.cpp.obj"
+  "C:/Users/kosar/CLionProjects/rubiksCube/Red.cpp" "C:/Users/kosar/CLionProjects/rubiksCube/cmake-build-debug/CMakeFiles/rubiksCube.dir/Red.cpp.obj"
+  "C:/Users/kosar/CLionProjects/rubiksCube/SolvingAlgorithm.cpp" "C:/Users/kosar/CLionProjects/rubiksCube/cmake-build-debug/CMakeFiles/rubiksCube.dir/SolvingAlgorithm.cpp.obj"
+  "C:/Users/kosar/CLionProjects/rubiksCube/main.cpp" "C:/Users/kosar/CLionProjects/rubiksCube/cmake-build-debug/CMakeFiles/rubiksCube.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/rubiksCube.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rubiksCube.dir/Blue.cpp.obj"
+  "CMakeFiles/rubiksCube.dir/Cube.cpp.obj"
+  "CMakeFiles/rubiksCube.dir/Green.cpp.obj"
+  "CMakeFiles/rubiksCube.dir/Orange.cpp.obj"
+  "CMakeFiles/rubiksCube.dir/Red.cpp.obj"
+  "CMakeFiles/rubiksCube.dir/SolvingAlgorithm.cpp.obj"
+  "CMakeFiles/rubiksCube.dir/main.cpp.obj"
+  "librubiksCube.dll.a"
+  "rubiksCube.exe"
+  "rubiksCube.exe.manifest"
+  "rubiksCube.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rubiksCube.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
